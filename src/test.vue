112142086a99d37b4928c0a413938e2516c8f700
rainingXie <347440251@qq.com>
2021-11-10 13:34:55 +0800

    11

---
+++ src/test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>{{ getText() }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      aa: '111',
+      mm: 'sss'
+    }
+  },
+  methods: {
+    getText() {
+      return this.aa
+    }
+    getText1() {
+      return this.aa
+    }
+  }
+}
+</script>
+
+<style></style>
